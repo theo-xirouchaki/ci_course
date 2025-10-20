a158aa98eb7af902c834211a76da1c0b98186d99
Theano Xirouchaki <theano.xirouchaki@dtc.ox.ac.uk>
2025-10-20 13:41:36 +0100

    fixed linting again

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,4 +16,4 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
-    assert ci_course.minimum("-1.2", "-3") == None
+    assert ci_course.minimum("-1.2", "-3") is None
